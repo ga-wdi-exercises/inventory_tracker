f59dc9f6f32263e5fd7316f9970da0cd17fca650
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-23 19:14:32 -0400

    Added module and controller

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 "use strict";
 
 (function(){
+  angular
+  .module("inventory", [])
+  .controller("inventory_controller", InventoryController);
 
+  function InventoryController(){
+    var vm = this;
+    vm.hello = "Hello, world!";
+  }
 })();
