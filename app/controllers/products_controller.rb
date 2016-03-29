f3f49067aabebbc334ac83d7ed93f28e217d8286
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-29 10:56:17 -0400

    Renders Product#index both as JSON and HTML

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
-
+    respond_to do |format|
+      format.html
+      format.json{ render json: Product.all }
+    end
   end
 
 end
